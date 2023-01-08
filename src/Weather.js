c8239318b0746820316439c5e31c88123052484a
Morgan <72710813+MCpracticesforskills@users.noreply.github.com>
2023-01-07 16:36:08 -0800

    Multiple API calls in one state
    Created the weatherData useState to hold all weather data instead of building a const & a use State for each api call.

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Conditions() {
-  const apiKey = "b791624f59276c950435fa9627cd9828";
-  let city = "New York";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  return (
-    <div className="Conditions">
-      <form className="Form">
-        <input
-          type="search"
-          placeholder="Search for a city..."
-          autoFocus="on"
-        />
-        <input type="submit" value="Search" />
-      </form>
-      <h1 className="pb-3 pt-4">New York</h1>
-      <h6>Wednesday 7:00AM </h6>
-      <h6>Mostly Cloudy</h6>
-      <div className="row mt-4">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-            alt="cloud with rain"
-          ></img>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
-          <span className="degrees">6</span>
-          <span className="units">°C</span>
-        </div>
-        <div className="col-6">
-          <h6>Precipitation: 20%</h6>
-          <h6>Humidity: 45% </h6>
-          <h6>Wind: 7 km/h</h6>
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain.png",
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Conditions">
+        <form className="Form">
+          <input
+            type="search"
+            placeholder="Search for a city..."
+            autoFocus="on"
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <h1 className="pb-3 pt-4">{weatherData.city}</h1>
+        <h6>Wednesday 7:00AM </h6>
+        <h6 className="text-capitalize">{weatherData.description}</h6>
+        <div className="row mt-4">
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description}></img>
+
+            <span className="degrees">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="units">°C</span>
+          </div>
+          <div className="col-6">
+            <h6>Humidity: {weatherData.humidity}% </h6>
+            <h6>Wind: {weatherData.wind} km/h</h6>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b791624f59276c950435fa9627cd9828";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return <p>Loading...</p>;
+  }
 }
