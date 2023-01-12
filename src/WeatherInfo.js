3d428cba1aa896f25649bf72f1a2ce40552c98c8
Morgan <72710813+MCpracticesforskills@users.noreply.github.com>
2023-01-11 17:18:13 -0800

    Why is the html not displaying from WeatherTemperature.js?
    Will ask slack channel for help becuase I don't understand why the WeatherTempaerture.js component is not showing. It is linked properly.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import "./Weather.css";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,9 +17,7 @@ export default function WeatherInfo(props) {
           <span className="WeatherIcon">
             <WeatherIcon code={props.data.icon} />
           </span>
-
-          <span className="degrees">{Math.round(props.data.temperature)}</span>
-          <span className="units">°C</span>
+          <WeatherTemperature celcius={props.data.temperature} />
         </div>
         <div className="col-6">
           <h6>Humidity: {props.data.humidity}% </h6>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,12 +2,36 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celcius");
+
+  function convertUnitFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertUnitCelcuis(event) {
+    event.preventDefault();
+    setUnit("celcius");
+  }
+
   if (unit === "celcius") {
     <div className="WeatherTemperature">
       <span className="degrees">{Math.round(props.celcius)}</span>
-      <span className="units">°C | °F </span>
+      <span className="units">
+        °C |{" "}
+        <a href="/" onClick={convertUnitFahrenheit}>
+          °F
+        </a>{" "}
+      </span>
     </div>;
   } else {
-    <p>Fahrenheit</p>;
+    let fahrenheit = (props.celcius * 9) / 5 + 32;
+    <div className="WeatherTemperature">
+      <span className="degrees">{Math.round(fahrenheit)}</span>
+      <span className="units">
+        °C |{" "}
+        <a href="/" onClick={convertUnitCelcuis}>
+          °F
+        </a>{" "}
+      </span>
+    </div>;
   }
 }
