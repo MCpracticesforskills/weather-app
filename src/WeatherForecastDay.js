8b6f164b9c00cf6e9367b96353b2e5f752952cac
Morgan <72710813+MCpracticesforskills@users.noreply.github.com>
2023-01-15 10:17:27 -0800

    STABLE version
    created functions to call days

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,17 +2,26 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function day() {
+    const date = new Date(props.data.dt * 1000);
+    const day = date.getDay();
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
   return (
     <div className="WeatherForecastDay">
-      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         {" "}
         <span className="WeatherForecast-max">
-          {props.data.temp.max}° |
+          {Math.round(props.data.temp.max)}° |
         </span>{" "}
         {""}
-        <span className="WeatherForecast-min">{props.data.temp.min}°</span>
+        <span className="WeatherForecast-min">
+          {Math.round(props.data.temp.min)}°
+        </span>
       </div>
     </div>
   );
